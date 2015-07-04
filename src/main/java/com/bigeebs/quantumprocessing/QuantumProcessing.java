092e8b0d277c50ef9ff89bdf78a5f9e46d46cab7
Andrew Ebert <big.eebs@gmail.com>
2015-07-03 19:31:19 -0500

    Added file structure

---
+++ src/main/java/com/bigeebs/quantumprocessing/QuantumProcessing.java
@@ -0,0 +1,39 @@
+package com.bigeebs.quantumprocessing;
+
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid=QuantumProcessing.MODID, name=QuantumProcessing.MODNAME, version=QuantumProcessing.MODVER) //Initialize mod
+public class QuantumProcessing {
+
+    //Set the ID of the mod
+    public static final String MODID = "qp";
+    //Set the "Name" of the Mod
+    public static final String MODNAME = "Quantum Processing";
+    //Set the version of the mod
+    public static final String MODVER = "1.8.0-1.0";
+
+    @Mod.Instance(QuantumProcessing.MODID) //Tell Forge what instance to use
+    public static QuantumProcessing instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
