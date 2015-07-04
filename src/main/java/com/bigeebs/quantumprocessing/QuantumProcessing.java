cde64ad2c05cce46ba952eeec61c5f651af8373c
Andrew Ebert <big.eebs@gmail.com>
2015-07-04 17:30:38 -0500

    Now loading mcmod.info

--- src/main/java/com/bigeebs/quantumprocessing/QuantumProcessing.java
+++ src/main/java/com/bigeebs/quantumprocessing/QuantumProcessing.java
@@ -6,17 +6,10 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid=QuantumProcessing.MODID, name=QuantumProcessing.MODNAME, version=QuantumProcessing.MODVER) //Initialize mod
+@Mod(modid="quantumprocessing", name="QuantumProcessing", version="1.8-1.0") //Initialize mod
 public class QuantumProcessing {
 
-    //Set the ID of the mod
-    public static final String MODID = "quantumprocessing";
-    //Set the "Name" of the Mod
-    public static final String MODNAME = "QuantumProcessing";
-    //Set the version of the mod
-    public static final String MODVER = "1.8.0-1.0";
-
-    @Mod.Instance(QuantumProcessing.MODID) //Tell Forge what instance to use
+    @Mod.Instance("quantumprocessing") //Tell Forge what instance to use
     public static QuantumProcessing instance;
 
     @Mod.EventHandler
