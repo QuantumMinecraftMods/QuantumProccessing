53f1ed22fe7db855d3ad50a44f7b8a42e5be19e6
Andrew Ebert <big.eebs@gmail.com>
2015-07-11 10:06:48 -0500

    Deletions

--- src/java/com/bigeebs/QuantumProcessing/QuantumProcessing.java
+++
@@ -1,7 +0,0 @@
-package com.bigeebs.QuantumProcessing;
-
-/**
- * Created by bigee_000 on 7/3/2015.
- */
-public class QuantumProcessing {
-}
--- src/java/com/bigeebs/QuantumProcessing/proxy/ServerProxy.java
+++
@@ -1,7 +0,0 @@
-package com.bigeebs.quantumprocessing.proxy;
-
-/**
- * Created by bigee_000 on 7/3/2015.
- */
-public class ServerProxy {
-}
